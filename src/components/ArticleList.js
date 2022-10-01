b0d7ae240b94f12b7f89803b2ceb8fadbca3ad10
DavidLim93 <lim.david93@gmail.com>
2022-10-01 21:01:31 +1000

    Complete

---
+++ src/components/ArticleList.js
@@ -0,0 +1,20 @@
+import Article from "./Article"
+
+function ArticleList ({posts}) {
+    const Array=posts.map((post) => 
+    {
+        return (
+            <Article 
+            key={post.id} 
+            title={post.title} 
+            date={post.date}
+            preview={post.preview} 
+            minutes={post.minutes} />
+    )
+    });
+    return <main>
+        {Array}
+    </main>
+}
+
+export default ArticleList;
